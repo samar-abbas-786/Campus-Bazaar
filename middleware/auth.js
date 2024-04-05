aa3082b087eb2135e53146f015db58e0847b2887
Samar Abbas <samarabbas172003@gmail.com>
2024-04-06 04:01:39 +0530

    nav responsive

--- middleware/auth.js
+++ middleware/auth.js
@@ -28,4 +28,22 @@ async function protect(req, res, next) {
   next();
 }
 
+// const jwt = require("jsonwebtoken");
+// // Middleware to verify JWT token
+// const protect = (req, res, next) => {
+//   const token = req.headers.authorization?.split(" ")[1];
+//   if (!token) {
+//     res.status(401).send("Authentication required.");
+//     return;
+//   }
+//   jwt.verify(token, "secret", (err, decoded) => {
+//     if (err) {
+//       res.status(403).send("Invalid token.");
+//       return;
+//     }
+//     req.user = decoded;
+//     next();
+//   });
+// };
+
 module.exports = { protect };
