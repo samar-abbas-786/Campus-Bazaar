0d0fef1293ce5b851ea8b4bc69d4ecd6b10c375b
Samar Abbas <samarabbas172003@gmail.com>
2024-05-02 20:56:27 +0530

    add sidebar

--- routes/productRoutes.js
+++
@@ -1,5 +0,0 @@
-const express = require("express");
-const routes = express.Router();
-const { productUpload } = require("../controllers/productController");
-
-routes.post("/upload", upload.single("productImage"), productUpload);
